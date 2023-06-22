4b236ca789e9d8c6179e7ed8dd91b99e494dfc63
deba <ddmasterdon@gmail.com>
2023-06-23 01:22:13 +0530

    modified build script

--- build.sh
+++ build.sh
@@ -20,13 +20,14 @@ mv $BUILD/$FILENAME.asm $BIN/$FILENAME.asm
 # mv dd.asm $BIN/$FILENAME.asm
 # echo $kernel
 
-if [ $kernel=="Darwin" ]; then
+if [ "$kernel" = "Darwin" ]; then
     # macOS (M1 Mac)
     echo "linking and loading in m1"
     as $BIN/$FILENAME.asm -o $BIN/$FILENAME.o
     ld $BIN/$FILENAME.o -o $BIN/$FILENAME -lSystem -syslibroot `xcrun -sdk macosx --show-sdk-path`
-elif [ "$kernel"=="Linux" ]; then
+elif [ "$kernel" = "Linux" ]; then
     # Linux (Raspberry Pi)
+    echo "linking and loading with Pi"
     as $BIN/$FILENAME.asm -o $BIN/$FILENAME.o-gdwaef64 -g3
     ld $BIN/$FILENAME.o -o $BIN/$FILENAME -arch -arm64
 else
